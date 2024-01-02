2afd95382cd79ea74812a72f1ee9e26da9213460
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-01-02 10:37:30 -0300

    refactor comments

--- AlocacaoFAT.c
+++ AlocacaoFAT.c
@@ -4,7 +4,7 @@
 
 #define CLUSTER_SIZE 4096 //Define o tamanho do cluster
 #define FAT_ENTRY_SIZE 4 
-#define MAX_CLUSTERS 10 //Quantidade máxima de "clusters"
+#define MAX_CLUSTERS 10 //Quantidade mÃ¡xima de "clusters"
 
 typedef struct {
     int value;
@@ -14,17 +14,17 @@ typedef struct {
 FatEntry fat[MAX_CLUSTERS];
 
 void initialize_fat() {
-    // Inicializando o gerador de números aleatórios com base no tempo atual
+    // Inicializando o gerador de nÃºmeros aleatÃ³rios com base no tempo atual
     srand((unsigned int)time(NULL));
 
-    // Inicializando a tabela FAT com uma cadeia contínua de clusters
+    // Inicializando a tabela FAT com uma cadeia contÃ­nua de clusters
     int i;
     for (i = 0; i < MAX_CLUSTERS - 1; i++) {
-        fat[i].value = rand() % 100;  // Valor aleatório entre 0 e 99
-        fat[i].next_cluster = i + 1;  // Próximo cluster na cadeia
+        fat[i].value = rand() % 100;  // Valor aleatÃ³rio entre 0 e 99
+        fat[i].next_cluster = i + 1;  // PrÃ³ximo cluster na cadeia
     }
 
-    // O último cluster na cadeia aponta para -1
+    // O Ãºltimo cluster na cadeia aponta para -1
     fat[MAX_CLUSTERS - 1].value = rand() % 100;
     fat[MAX_CLUSTERS - 1].next_cluster = -1;
 }
@@ -33,10 +33,10 @@ int main() {
     initialize_fat();
 
     // Imprimindo a tabela FAT inicializada
-    printf("Inicialização da tabela FAT:\n");
+    printf("InicializaÃ§Ã£o da tabela FAT:\n");
     int i;
     for (i = 0; i < MAX_CLUSTERS; i++) {
-        printf("Cluster %d: Valor: %d, Próximo Cluster: %d\n", i, fat[i].value, fat[i].next_cluster);
+        printf("Cluster %d: Valor: %d, PrÃ³ximo Cluster: %d\n", i, fat[i].value, fat[i].next_cluster);
     }
     printf("\n");
 
